51bd8b70cad75ef9762cc7ea50b2b7b5bc445145
kevinFernandez <kevinfernandez.peinado@gmail.com>
2025-06-02 17:50:23 -0500

    feat: agregando conexion a base de datos SQlit

---
+++ dba.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+#Conectar con la base de datos, sino existe la crea
+conexion = sqlite3.connect('usuarios.db')
+
+#Cursor para comando sql
+cursor = conexion.cursor();
+
+#Crear tabla
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS usuarios (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    usuario TEXT NOT NULL UNIQUE,
+    contrasena TEXT NOT NULL
+)
+""")
+
+#Guardar y cerrar
+conexion.commit()
+conexion.close()
+
+print('Base de datos creada correctamente')
